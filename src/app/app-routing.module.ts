953683b805141763b5f326fe3d2882595f860a51
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-07-19 14:42:50 +0200

    init lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from './modules/general/notfound/notfound.component';
+import { HomeComponent } from './modules/general/home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'about', loadChildren: './modules/general/about/about.module#AboutModule'
+  },
+  {
+    path: 'contact', loadChildren: './modules/general/contact/contact.module#ContactModule'
+  },
+  {
+    path: '**', component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { HomeComponent } from './modules/general/home/home.component';
+import { NotFoundComponent } from './modules/general/notfound/notfound.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
